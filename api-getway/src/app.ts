5a897227464d3aaf6c9aec74876cf9a888d8f087
TANHEHANZ <hantach10@gmail.com>
2025-03-20 17:30:15 -0400

    feat(configure):configuracion logger y manejo de errores en la entrada de los microservicios

---
+++ api-getway/src/app.ts
@@ -0,0 +1,8 @@
+import config from "./config/config";
+import { createServer } from "./server";
+
+const server = createServer();
+
+server.listen(config.port, () => {
+  console.log(`API-GETWAY Run in :  ${config.port}`);
+});
