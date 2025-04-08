12e18041de13981b3c05cd7579d90882c6f34821
TANHEHANZ <hantach10@gmail.com>
2025-04-08 10:50:31 -0400

    refact(types): manejo de types globales segun OAuth02

--- authentication-service/src/infraestructure/global.d.ts
+++ authentication-service/src/infraestructure/global.d.ts
@@ -1,11 +1,18 @@
-import { UserRole } from "@prisma/client";
+import { User } from "@prisma/client";
 import { AuthPayload } from "./types/jwt";
+import { OAuthClient } from "./types/oauth.types";
+import { AuthenticationContext } from "./types/internal.types";
+
 export {};
 declare global {
   namespace Express {
     interface Request {
       user?: User;
       auth?: AuthPayload;
+      oauth?: {
+        client: OAuthClient;
+        context?: AuthenticationContext;
+      };
     }
   }
 }
