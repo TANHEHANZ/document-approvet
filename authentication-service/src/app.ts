1b46671b9fe4e193f18c05af59244234fbf45602
TANHEHANZ <hantach10@gmail.com>
2025-04-23 16:04:10 -0400

    feat(email):email service

--- authentication-service/src/app.ts
+++ authentication-service/src/app.ts
@@ -3,11 +3,13 @@ dotenv.config();
 import { createServer } from "./server";
 import config from "./infraestructure/config/config";
 import { redisService } from "./infraestructure/lib/redis";
+import { connectEmail } from "./infraestructure/lib/email/services/conection.email";
 
 const startServer = async () => {
   try {
     const server = createServer();
     await redisService.init();
+    await connectEmail();
     server.listen(config.port, () => {
       console.log(`SERVICE-AUTH Running on port: ${config.port}`);
     });
